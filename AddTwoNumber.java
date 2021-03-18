f861847eac81916870418362699fa4a1a62f53b8
Abhijeet Anand <aabhijeet011@gmail.com>
2021-03-18 09:12:03 +0530

    u.1.1

---
+++ AddTwoNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AddTwoNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter the first No : " );
+        int n = sc.nextInt();
+        System.out.println("Please Enter the second No : ");
+        int m = sc.nextInt();
+        System.out.println("The sum of two Number is: " + (n+m));
+        sc.close();
+
+    }
+    
+}
